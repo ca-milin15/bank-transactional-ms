48d20360a1cca5304ced4a534a872d3f5eb9a103
User Developer <backenddeveloper30@gmail.com>
2024-06-05 22:30:51 -0500

    Estructura inicial del proyecto

---
+++ src/main/java/com/pichincha/bank_transactional_ms/BankTransactionalMsApplication.java
@@ -0,0 +1,13 @@
+package com.pichincha.bank_transactional_ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankTransactionalMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankTransactionalMsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pichincha/bank_transactional_ms/BankTransactionalMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pichincha.bank_transactional_ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankTransactionalMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
